f87c8cf6d4cec135084ba3937ff1ddab4127cf22
M. Hasan <mohiuddinkhasherhat@gmail.com>
2023-02-17 09:34:12 +0600

    V1.2 - Added output value of hsl, rgb, and hex

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ const colorUpdate = () => {
     let boxWidth = color_box.offsetWidth;
     let hue_temp = hue.value;
     
-    addClass(selected, ["border-2", "pointer-events-none", "border-white", "bg-transparent", "h-4", "w-4", "rounded-full", "absolute", "z-10", "-translate-x-1/2", "-translate-y-1/2", "marker"])
+    addClass(selected, ["border-2", "pointer-events-none", "border-white", "bg-transparent", "h-4", "w-4", "rounded-full", "absolute", "z-10", "-translate-x-1/2", "-translate-y-1/2", "marker", "shadow-gray-400", "shadow-md"])
     selected.style.top = `${clickY}px`;
     selected.style.left = `${clickX}px`;
 
@@ -60,5 +60,34 @@ const addClass = (item, classlist) => {
 const colorCalc = (hue_temp, saturation_temp, light_temp) => {
     let saturation = saturation_temp;
     let light = Math.round(100 * (100-light_temp) / (100 + saturation_temp));
-    return `hsl(${hue_temp}, ${saturation}%, ${light}%)`;
+    let hue = hue_temp;
+    textUpdate(hue, saturation, light);
+    return `hsl(${hue}, ${saturation}%, ${light}%)`;
 };
+
+const textUpdate = (hue, saturation, light) => {
+    document.querySelector(".color_hsl").innerHTML = `hsl(${hue}, ${saturation}%, ${light}%)`;
+    let colorHex = hslToHex(hue, saturation, light);
+    document.querySelector(".color_hex").innerHTML = colorHex;
+    let colorRgb = hexToRgb(colorHex);
+    document.querySelector(".color_rgb").innerHTML = colorRgb;
+}
+
+const hslToHex = (hue, saturation, light) => {
+    light /= 100;
+    const temp = saturation * Math.min(light, 1-light) / 100;
+    const converter = input => {
+        const temp2 = (input + hue / 30) % 12;
+        const color = light - temp * Math.max(Math.min(temp2 - 3, 9 - temp2, 1), -1);
+        return Math.round(255 * color).toString(16).padStart(2, "0");
+    };
+    return `#${converter(0)}${converter(8)}${converter(4)}`;
+}
+
+const hexToRgb = hex => {
+    let red = parseInt(hex.slice(1,3), 16);
+    let green = parseInt(hex.slice(3,5), 16);
+    let blue = parseInt(hex.slice(5,7), 16);
+    console.log(hex.slice(1,3), hex.slice(3,5), hex.slice(5,7));
+    return `rgb(${red}, ${green}, ${blue})`
+}
